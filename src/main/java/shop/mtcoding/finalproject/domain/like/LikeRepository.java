160f7718d0ca4c9452684887f4f41672d56ca6bb
Gammi06 <bpudgw182@naver.com>
2022-12-09 16:16:48 +0900

    좋아요 추가, 삭제 기능구현 완료 및 Junit Test 완료

--- src/main/java/shop/mtcoding/finalproject/domain/like/LikeRepository.java
+++ src/main/java/shop/mtcoding/finalproject/domain/like/LikeRepository.java
@@ -8,4 +8,8 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
     @Query(value = "select count(id) count from likes l where l.store_id = :storeId", nativeQuery = true)
     LikeInterface findByStoreId(@Param("storeId") Long storeId);
+
+    @Query(value = "select * from likes l left outer join users u on u.id = l.user_id left outer join stores s on s.id = l.store_id where l.user_id = :userId and l.store_id = :storeId", nativeQuery = true)
+    Like findByUserIdAndStoreId(@Param("userId") Long userId, @Param("storeId") Long storeId);
+
 }
