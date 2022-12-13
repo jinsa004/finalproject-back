6bf6d2a37757417835edce8eed46df633a04d62f
jinsa004 <112357294+jinsa004@users.noreply.github.com>
2022-12-13 10:58:11 +0900

    찜한가게 목록보기 기능 (#61)
    * 신고리뷰 목록보기, 상세보기, 처리하기 기능 구현 (#56)

--- src/main/java/shop/mtcoding/finalproject/domain/like/LikeRepository.java
+++ src/main/java/shop/mtcoding/finalproject/domain/like/LikeRepository.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.finalproject.domain.like;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,7 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     @Query(value = "select * from likes l left outer join users u on u.id = l.user_id left outer join stores s on s.id = l.store_id where l.user_id = :userId and l.store_id = :storeId", nativeQuery = true)
     Like findByUserIdAndStoreId(@Param("userId") Long userId, @Param("storeId") Long storeId);
 
+    // 찜한 가게 목록보기 기능
+    @Query("select l from Like l join fetch l.store s where l.user.id = :userId")
+    List<Like> findByUserIdToLikeStore(@Param("userId") Long userId);
 }
--- src/main/java/shop/mtcoding/finalproject/dto/ResponseDto.java
+++ src/main/java/shop/mtcoding/finalproject/dto/ResponseDto.java
@@ -2,9 +2,11 @@ package shop.mtcoding.finalproject.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 // http code = 200(get, delete, put), 201(post)
 @RequiredArgsConstructor
+@Setter
 @Getter
 public class ResponseDto<T> {
     private final String msg;
