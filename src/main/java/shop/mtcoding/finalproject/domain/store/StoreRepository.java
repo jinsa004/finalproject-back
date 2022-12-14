78c437c2cb3d5cc98bfbc0dbfed384868e6e4170
jinsa004 <jinsa004@gmail.com>
2022-12-14 10:02:34 +0900

    입점신청중인 store 목록보기에 뜨는 현상 쿼리수정으로 해결

--- src/main/java/shop/mtcoding/finalproject/domain/store/StoreRepository.java
+++ src/main/java/shop/mtcoding/finalproject/domain/store/StoreRepository.java
@@ -12,10 +12,10 @@ import shop.mtcoding.finalproject.config.enums.StoreCategoryEnum;
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     // 카테고리별 가게목록보기
-    @Query("select s from Store s where category = :category and isClosure = false")
+    @Query("select s from Store s where category = :category and isClosure = false and isAccept = true")
     List<Store> findAllByCategory(@Param("category") StoreCategoryEnum category);
 
-    @Query("select s from Store s join fetch s.user u where s.user.id = :userId and s.isClosure = false")
+    @Query("select s from Store s join fetch s.user u where s.user.id = :userId and s.isClosure = false and isAccept = true")
     Optional<Store> findByUserId(@Param("userId") Long userId);
 
     @Query("select s from Store s where id = :id and s.isClosure = false")
