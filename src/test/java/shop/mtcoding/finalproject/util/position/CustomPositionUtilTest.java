4768c4613c152c91cd2fefa18bfc915337bc7a6b
Gammi06 <bpudgw182@naver.com>
2022-12-14 16:45:59 +0900

    주변좌표알아오기기능 구상함

---
+++ src/test/java/shop/mtcoding/finalproject/util/position/CustomPositionUtilTest.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.finalproject.util.position;
+
+public class CustomPositionUtilTest {
+
+    public static void 내위치근처동이름알아오기() {
+
+        // 1. 테스트용 경도, 위도 좌표 짜기 (사용자 위치)
+
+        // 2. 바운드 만들기
+
+        // 3. 바운드 내에 대표 좌표가 있는지 체크하기
+
+        // 4. 들어온 동 대표 좌표 리턴하기
+    }
+
+}
---
+++ src/test/java/shop/mtcoding/finalproject/util/position/PositionEnumTest.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.finalproject.util.position;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PositionEnumTest {
+    BUSANSASANGJURAEDONG("35.1513888/129.0011741"),
+    BUSANSASANGDUKPODONG("35.1703825/128.9832204");
+
+    private String adress;
+}
