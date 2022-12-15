a70fb514832103aecfa2cfe586fd5660e3202890
jinsa004 <112357294+jinsa004@users.noreply.github.com>
2022-12-15 14:38:24 +0900

    프론트요청]입점대기유저/입점미신청자/가게등록유저/가게미등록유저 구분을 위한 로직추가 (#91)
    * 프론트요청]사장님 웹 대기화면에서 유저 역할 추가
    
    * 입점대기자와 입점신청을 안한 사람과의 차이를 위한 로직추가
    
    * 프론트요청]입점대기유저와 입점미신청자 구분을 위한 로직추가 (#90)
    
    * 프론트요청]입점대기유저와 입점미신청자 구분을 위한 로직추가 (#89)
    
    * 입점대기자와 입점신청을 안한 사람과의 차이를 위한 로직추가
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>
    Co-authored-by: Gammi06 <bpudgw182@naver.com>
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>
    Co-authored-by: Gammi06 <bpudgw182@naver.com>
    
    * 사장님 웹 로그인 로직처리 최종안
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>
    Co-authored-by: Gammi06 <bpudgw182@naver.com>

--- src/main/java/shop/mtcoding/finalproject/dto/customerReview/CustomerReviewRespDto.java
+++ src/main/java/shop/mtcoding/finalproject/dto/customerReview/CustomerReviewRespDto.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.Setter;
-import shop.mtcoding.finalproject.config.exception.CustomApiException;
 import shop.mtcoding.finalproject.domain.customerReview.CustomerMenuInterface;
 import shop.mtcoding.finalproject.domain.customerReview.CustomerReview;
 import shop.mtcoding.finalproject.domain.customerReview.CustomerReviewInterface;
